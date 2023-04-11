0f935711a4ad2cbbc02cfc46cdb906d58296e315
Manish Kumar <87971964+manishkumar30@users.noreply.github.com>
2023-04-11 22:11:06 +0530

    Create Median of the subarrays.cpp

---
+++ Median of the subarrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+  long long help(int n, vector<int> arr, int m){
+      unordered_map<int, int> mm;
+      mm[0]++;
+      int sum=0;
+      long long ans=0, currElement=0;
+      for(int i=0;i<n;i++){
+          if(arr[i]<m){
+              sum--;
+              currElement-=mm[sum];
+          }
+          else{
+              currElement+=mm[sum];
+              sum++;
+          }
+          ans+=currElement;
+          mm[sum]++;
+      }
+      return ans;
+  }
+  
+    long long countSubarray(int N,vector<int> A,int M) {
+        // code here
+        long long a=help(N,A,M);
+        long long b=help(N,A,M+1);
+        return a-b;
+    }
+};
