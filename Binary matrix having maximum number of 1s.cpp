3b575927cf0a68793d175f625720fc9e6940ba26
Manish Kumar <87971964+manishkumar30@users.noreply.github.com>
2023-04-11 22:05:00 +0530

    Create Binary matrix having maximum number of 1s.cpp

---
+++ Binary matrix having maximum number of 1s.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findMaxRow(vector<vector<int>> mat, int N) {
+        //code here
+        int i=0;
+        int j=N-1;
+        int row=0;
+        int count=0;
+        while(j>=0 and i<N)
+        {
+            if(mat[i][j]==1){
+                count++;
+                row=i;
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(row);
+        ans.push_back(count);
+        
+        return ans;
+    }
+};
